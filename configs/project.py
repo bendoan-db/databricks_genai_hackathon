087fa0b58a48c6d23045e5aa80d558e51055e86a
fflory <fflory>
2025-03-28 23:47:04 +0000

    up

--- configs/project.py
+++ configs/project.py
@@ -11,6 +11,7 @@ class Environment(BaseModel):
 
 
 class VectorSearchIndexAttributes(Environment):
+    endpoint_name: Optional[str]
     index_name: Optional[str]
     source_table_name: Optional[str]
     primary_key: str
@@ -20,6 +21,7 @@ class VectorSearchIndexAttributes(Environment):
 
 
 class ProjectConfig(Environment):
+    
     vector_search_attributes: Dict[str, Dict[str, Any]]  # use Dict[str, Any] to allow pre-validation
     
     @field_validator("vector_search_attributes", mode="before")
@@ -31,14 +33,14 @@ class ProjectConfig(Environment):
         for key, nested in v.items():
             if not isinstance(nested, dict):
                 continue
-            if "vector_search_endpoint_name" not in nested:
-                nested["vector_search_endpoint_name"] = vs_endpoint
-            if "embedding_model_endpoint_name" not in nested:
-                nested["embedding_model_endpoint_name"] = emb_endpoint
+            if "endpoint_name" not in nested:
+                nested["endpoint_name"] = vs_endpoint
             if "index_name" not in nested:
                 nested["index_name"] = f"{uc_catalog}.{uc_schema}.{key}_index"
             if "source_table_name" not in nested:
                 nested["source_table_name"] = f"{uc_catalog}.{uc_schema}.{key}"
+            if "embedding_model_endpoint_name" not in nested:
+                nested["embedding_model_endpoint_name"] = emb_endpoint
             if "pipeline_type" not in nested:
                 nested["pipeline_type"] = "TRIGGERED"
         return v
