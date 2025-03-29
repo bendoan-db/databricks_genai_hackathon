b21b6f890b54bdfa578c84066f1205302abeeb97
fflory <fflory>
2025-03-29 16:00:28 +0000

    update genie table pydantic model

--- configs/project.py
+++ configs/project.py
@@ -1,70 +1,96 @@
-from pydantic import BaseModel, field_validator, computed_field
+from pydantic import BaseModel, field_validator, computed_field, model_validator, Field
 from typing import Dict, Any, Optional
 
-
-class Environment(BaseModel):
+class UnityCatalog(BaseModel):
     uc_catalog: str
     uc_schema: str
+
+class UCVolume(UnityCatalog):
     raw_data_volume: str
+
+class Environment(UCVolume):
+    # uc_catalog: str
+    # uc_schema: str
+    # raw_data_volume: str
     vector_search_endpoint_name: str
-    embedding_endpoint_name: str  
+    embedding_endpoint_name: str
+    genie_space_id: str
 
 
-class VectorSearchIndexAttributes(Environment):
-    endpoint_name: Optional[str]
-    index_name: Optional[str]
-    source_table_name: Optional[str]
-    primary_key: str
-    embedding_source_column: str
-    embedding_model_endpoint_name: Optional[str]
-    pipeline_type: Optional[str]
+# class VectorSearchIndexAttributes(Environment):
+#     endpoint_name: Optional[str]
+#     index_name: Optional[str]
+#     source_table_name: Optional[str]
+#     primary_key: str
+#     embedding_source_column: str
+#     embedding_model_endpoint_name: Optional[str]
+#     pipeline_type: Optional[str]
 
-class GenieTablesAttributes(Environment):
-    fqn: Optional[str]
+class GenieTableAttributes(UCVolume):
+
+    # Override inherited required fields to be optional here.
+    uc_catalog: Optional[str] = None
+    uc_schema: Optional[str] = None
+    raw_data_volume: Optional[str] = None
+
+    name: Optional[str] = Field(
+        default=None,
+        description="This field holds the name of the table. Not the fully qualified name, just the actual table name. It will likely be equivalent to the corresponding dictionary key."
+    )
     url: str
-    local_path: Optional[str]
-    # description: str
-    # primary_key: str
-
+    fqn: Optional[str] = None
+    local_path: Optional[str] = None
 
 class ProjectConfig(Environment):
 
-    genie_tables_attributes: Dict[str, Dict[str, Any]]
+    genie_tables: Dict[str, GenieTableAttributes]
 
-    @field_validator("genie_tables_attributes", mode="before")
-    def fill_genie_tables_attributes(cls, v, info):
-        uc_catalog = info.data.get("uc_catalog")
-        uc_schema = info.data.get("uc_schema")
-        raw_data_volume = info.data.get("raw_data_volume")
-        for key, nested in v.items():
-            if not isinstance(nested, dict):
-                continue
-            if "name" not in nested:
-                nested["fqn"] = f"{uc_catalog}.{uc_schema}.{key}"
-            if "local_path" not in nested:
-                nested["local_path"] = f"/Volumes/{uc_catalog}/{uc_schema}/{raw_data_volume}/{key}.snappy.parquet"
-        return v
+    @model_validator(mode="after")
+    def impute_genie_tables(cls, model: "ProjectConfig") -> "ProjectConfig":
+        # For each genie table, if a value is None, impute he parent's value.
+        for key, table in model.genie_tables.items():
+            if table.uc_catalog is None:
+                table.uc_catalog = model.uc_catalog
+            if table.uc_schema is None:
+                table.uc_schema = model.uc_schema
+            if table.raw_data_volume is None:
+                table.raw_data_volume = model.raw_data_volume
+            if table.name is None:
+                table.name = key
+            if table.fqn is None:
+                table.fqn = f"{table.uc_catalog}.{table.uc_schema}.{table.name}"
+            if table.local_path is None:
+                table.local_path = f"/Volumes/{table.uc_catalog}/{table.uc_schema}/{table.raw_data_volume}/{table.name}.snappy.parquet" 
+        return model
 
     
-    vector_search_attributes: Dict[str, Dict[str, Any]]  # use Dict[str, Any] to allow pre-validation
+    # vector_search_attributes: Dict[str, Dict[str, Any]]  # use Dict[str, Any] to allow pre-validation
+    # use the table name as the key for the vector search attributes
+    # vector_search_attributes:
+    #   <table_name1>:
+    #     primary_key: <primary_key_column>
+    #     embedding_source_column: <content_column>
+    #   <table_name2>:
+    #     primary_key: <primary_key_column>
+    #     embedding_source_column: <content_column>
     
-    @field_validator("vector_search_attributes", mode="before")
-    def fill_vector_search_attributes(cls, v, info):
-        uc_catalog = info.data.get("uc_catalog")
-        uc_schema = info.data.get("uc_schema")
-        vs_endpoint = info.data.get("vector_search_endpoint_name")
-        emb_endpoint = info.data.get("embedding_endpoint_name")
-        for key, nested in v.items():
-            if not isinstance(nested, dict):
-                continue
-            if "endpoint_name" not in nested:
-                nested["endpoint_name"] = vs_endpoint
-            if "index_name" not in nested:
-                nested["index_name"] = f"{uc_catalog}.{uc_schema}.{key}_index"
-            if "source_table_name" not in nested:
-                nested["source_table_name"] = f"{uc_catalog}.{uc_schema}.{key}"
-            if "embedding_model_endpoint_name" not in nested:
-                nested["embedding_model_endpoint_name"] = emb_endpoint
-            if "pipeline_type" not in nested:
-                nested["pipeline_type"] = "TRIGGERED"
-        return v
+    # @field_validator("vector_search_attributes", mode="before")
+    # def fill_vector_search_attributes(cls, v, info):
+    #     uc_catalog = info.data.get("uc_catalog")
+    #     uc_schema = info.data.get("uc_schema")
+    #     vs_endpoint = info.data.get("vector_search_endpoint_name")
+    #     emb_endpoint = info.data.get("embedding_endpoint_name")
+    #     for key, nested in v.items():
+    #         if not isinstance(nested, dict):
+    #             continue
+    #         if "endpoint_name" not in nested:
+    #             nested["endpoint_name"] = vs_endpoint
+    #         if "index_name" not in nested:
+    #             nested["index_name"] = f"{uc_catalog}.{uc_schema}.{key}_index"
+    #         if "source_table_name" not in nested:
+    #             nested["source_table_name"] = f"{uc_catalog}.{uc_schema}.{key}"
+    #         if "embedding_model_endpoint_name" not in nested:
+    #             nested["embedding_model_endpoint_name"] = emb_endpoint
+    #         if "pipeline_type" not in nested:
+    #             nested["pipeline_type"] = "TRIGGERED"
+    #     return v
