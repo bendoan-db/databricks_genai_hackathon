715c69e049dfb03822750e5c4d4b260c287d341d
fflory <fflory>
2025-04-05 21:53:57 +0000

    tested in different uc_schema

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,16 @@
-#Create mlflow experiment
 import mlflow
 from databricks.sdk import WorkspaceClient
-
-import sys, os
+import sys, os, yaml
 sys.path.append(os.path.abspath('..'))
 from configs.project import ProjectConfig
-import yaml
 
 with open("../configs/project.yml", "r") as file:
     data = yaml.safe_load(file)
 
 projectConfig = ProjectConfig(**data)
-# projectConfig.dict()
+projectConfig.dict()
 
 mlflow_experiment_base_path = projectConfig.mlflow_experiment_base_path
-
 
 def set_mlflow_experiment(experiment_tag):
     w = WorkspaceClient()
