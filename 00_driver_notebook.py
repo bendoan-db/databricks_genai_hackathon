463679dd494f8af75cf08b557ea0f5b829d90c7f
bendoan-db <bendoan-db>
2025-06-16 02:44:39 +0000

    add planning agent

--- 00_driver_notebook.py
+++ 00_driver_notebook.py
@@ -15,11 +15,10 @@
 import os
 from dbruntime.databricks_repl_context import get_context
 
-TOKEN = get_context().apiToken
 HOSTNAME = get_context().browserHostName
 USERNAME = get_context().user
 
-os.environ['DATABRICKS_TOKEN'] = TOKEN
+os.environ['DATABRICKS_TOKEN'] = dbutils.secrets.get(scope="doan-demos", key="databricks-pat")
 os.environ['DATABRICKS_URL'] = get_context().apiUrl
 
 # COMMAND ----------
@@ -38,6 +37,7 @@ catalog=databricks_config['catalog']
 schema=databricks_config['schema']
 mlflow_experiment=databricks_config['mlflow_experiment']
 eval_table=databricks_config['eval_table_name']
+model_name=databricks_config['model_name']
 
 #load vs configs
 vector_search_endpoint = retriever_config['vector_search_endpoint']
@@ -68,7 +68,7 @@ mlflow.set_experiment(experiment_fqdn)
 
 # COMMAND ----------
 
-# MAGIC %run ./01a_unstructured_retrieval_agent
+# MAGIC %run ./03_deep_research_agent
 
 # COMMAND ----------
 
@@ -82,7 +82,7 @@ example_input = {
         "messages": [
             {
                 "role": "user",
-                "content": "What was AAPL's most popular product in 2022?",
+                "content": "How did APPL's operating margin change between 2020 and 2021? What factors contributed to this?",
             }
         ]
     }
@@ -112,12 +112,7 @@ display(eval_dataset)
 
 # COMMAND ----------
 
-figure_correctness_guideline = """You are an impartial judge tasked with verifying the numerical accuracy of a generated response compared to a ground truth value. Your goal is to determine whether the generated number is numerically correct within two decimal places of the ground truth.
-
-Evaluation Rules:
-* Round both the generated value and the ground truth to two decimal places.
-* If the two rounded values are exactly equal, the answer is Correct.
-* If the rounded values differ, the answer is Incorrect."""
+structure = """The response must use clear, concise language and structures responses logically. It avoids jargon or explains technical terms when used."""
 
 # COMMAND ----------
 
@@ -152,7 +147,7 @@ eval_results = mlflow.genai.evaluate(
         RelevanceToQuery(),
         Safety(),
         figure_correctness,
-        Guidelines(name="figure_correctness_guideline", guidelines=figure_correctness_guideline),
+        Guidelines(name="structure", guidelines=structure),
     ],
 )
 
@@ -174,12 +169,12 @@ from mlflow.models.resources import (
   DatabricksUCConnection
 )
 
-with mlflow.start_run(run_name="sec_rag_doan"):
+with mlflow.start_run():
     logged_chain_info = mlflow.pyfunc.log_model(
         python_model=os.path.join(os.getcwd(), "01a_unstructured_retrieval_agent"),
         model_config=os.path.join(os.getcwd(), "configs/agent.yaml"), 
-        artifact_path="agent",  # Required by MLflow
-        code_paths=[os.path.join(os.getcwd(), "tools")],
+        name=model_name,  # Required by MLflow
+        code_paths=[os.path.join(os.getcwd(), "vector_search_utils")],
         input_example=example_input,
         resources=[
         DatabricksVectorSearchIndex(index_name=f"{catalog}.{schema}.{vector_search_index}"),
@@ -194,71 +189,12 @@ with mlflow.start_run(run_name="sec_rag_doan"):
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC # Evaluate
-
-# COMMAND ----------
-
-eval_df = spark.sql("select * from doan.finance_bench.eval_data_gold")
-display(eval_df)
-
-# COMMAND ----------
-
-eval_sample = eval_df.limit(10)
-
-# COMMAND ----------
-
-# MAGIC %run ./judge_examples
-
-# COMMAND ----------
-
-display(examples_df)
-
-# COMMAND ----------
-
-# set evaluation experiment
-# mlflow.set_experiment("/Users/forrest.murray@databricks.com/sec_rag")
-
-# global_guidelines = {
-#     "figure_correctness": [
-#         "If the question asks for a figure, the generated response should contain the correct figure that's listed in the ground truth. The figure should be accurate within 1 decimal point."
-#     ],
-# }
-
-
-with mlflow.start_run(run_name="sec_rag_agent_doan_with_judge_examples"):
-    # Evaluate
-    eval_results = mlflow.evaluate(
-        data=eval_df.toPandas(),
-        model=f"runs:/{logged_chain_info.run_id}/agent",  # replace `agent` with artifact_path that you used when calling log_model.  By default, this is `agent`.
-        model_type="databricks-agent",
-        evaluator_config={
-            "databricks-agent": {
-                "metrics": [
-                    "chunk_relevance",
-                    "context_sufficiency",
-                    "correctness",
-                    "safety",
-                    "groundedness",
-                    #"document_recall"
-                ],
-                "examples_df": examples_df
-            }
-        },
-    )
-
-# COMMAND ----------
-
-eval_results.tables['eval_results']
-
-# COMMAND ----------
-
-# MAGIC %md
 # MAGIC # Deploy
 
 # COMMAND ----------
 
 mlflow.models.predict(
-    model_uri=f"runs:/{logged_chain_info.run_id}/agent",
+    model_uri=f"runs:/{logged_chain_info.run_id}/{model_name}",
     input_data=example_input,
     env_manager="uv",
 )
@@ -266,11 +202,6 @@ mlflow.models.predict(
 # COMMAND ----------
 
 mlflow.set_registry_uri("databricks-uc")
-
-# TODO: define the catalog, schema, and model name for your UC model
-catalog = "doan"
-schema = "finance_bench"
-model_name = "sec_rag_agent"
 UC_MODEL_NAME = f"{catalog}.{schema}.{model_name}"
 
 # register the model to UC
@@ -281,7 +212,17 @@ uc_registered_model_info = mlflow.register_model(
 # COMMAND ----------
 
 from databricks import agents
-agents.deploy(UC_MODEL_NAME, uc_registered_model_info.version)
+
+agents.deploy(
+    model_name=UC_MODEL_NAME,
+    model_version=uc_registered_model_info.version,
+    environment_vars={
+        "DATABRICKS_URL": get_context().apiUrl,
+        "DATABRICKS_TOKEN": dbutils.secrets.get(
+            scope="doan-demos", key="databricks-pat"
+        ),
+    },
+)
 
 # COMMAND ----------
 
